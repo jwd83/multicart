8f903428a18c67d5619aabb071ce31973e64adfc
Jared De Blander <jared0x90@gmail.com>
2024-02-26 04:03:00 -0500

    release builder is working pretty well!

--- build-release.bat
+++ build-release.bat
@@ -30,8 +30,43 @@ REM rename the dist folder to JackGamesMulticart
 ren dist JackGamesMulticart
 
 REM compress the JackGamesMulticart folder into a zip file using 7z
-REM include the date and time in the zip file name
-7z a -tzip JackGamesMulticart.zip JackGamesMulticart
+REM include the current timestamp in the zip file name
 
-REM remove the JackGamesMulticart folder
+REM This script taken from the following URL:
+REM http://www.winnetmag.com/windowsscripting/article/articleid/9177/windowsscripting_9177.html
+
+REM Create the date and time elements.
+for /f "tokens=1-7 delims=:/-, " %%i in ('echo exit^|cmd /q /k"prompt $d $t"') do (
+   for /f "tokens=2-4 delims=/-,() skip=1" %%a in ('echo.^|date') do (
+      set dow=%%i
+      set %%a=%%j
+      set %%b=%%k
+      set %%c=%%l
+      set hh=%%m
+      set min=%%n
+      set ss=%%o
+   )
+)
+
+REM make hour 2 digits
+if %hh%==0 set hh=00
+if %hh%==1 set hh=01
+if %hh%==2 set hh=02
+if %hh%==3 set hh=03
+if %hh%==4 set hh=04
+if %hh%==5 set hh=05
+if %hh%==6 set hh=06
+if %hh%==7 set hh=07
+if %hh%==8 set hh=08
+if %hh%==9 set hh=09
+
+REM Let's see the result.
+echo ------------------------------------------------------
+echo Build stage finished, packing zip at %dow% %yy%-%mm%-%dd% @ %hh%:%min%:%ss%
+echo ------------------------------------------------------
+7z a -tzip JackGamesMulticart-%yy%-%mm%-%dd%-at-%hh%-%min%-%ss%.zip JackGamesMulticart
+
+REM remove the old build, dist and JackGamesMulticart folders if they are still around
+rmdir /s /q dist
+rmdir /s /q build
 rmdir /s /q JackGamesMulticart
