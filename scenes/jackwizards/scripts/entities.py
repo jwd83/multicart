020eec674c104eb0d6415614f5ff7952714aaa95
Jared De Blander <jared@ayrscott.com>
2024-03-18 22:16:11 -0400

    started an entities class for this game

---
+++ scenes/jackwizards/scripts/entities.py
@@ -0,0 +1,15 @@
+import pygame
+from utils import SpriteSheet, Animation, load_tpng_folder
+
+
+class Entity:
+    def __init__(self, centerx, centery):
+        pass
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+
+class Monster(Entity):
+    def __init__(self):
+        super().__init__()
