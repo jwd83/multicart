4350593beab8d1e8eaac8032c5526854e0b34ecc
Jared De Blander <jared@ayrscott.com>
2024-03-20 04:03:02 -0400

    made a little Seed utility class to generate reliably seeded randomness.

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import os
+from hashlib import md5
 
 
 # load a single image
@@ -117,3 +118,91 @@ class Animation:
 
     def img(self):
         return self.images[int(self.frame / self.img_duration)]
+
+class Seed:
+    """
+    This class represents a Seed object.
+
+    Attributes:
+        __seed (str): The seed value. Default is "Peach".
+
+    Methods:
+        get_seed(): Returns the current seed value.
+        set_seed(seed: str): Sets a new seed value.
+    """
+
+    def __init__(self, seed: str = "Peach"):
+        """
+        The constructor for Seed class.
+
+        Parameters:
+            seed (str): The seed value. Default is "Peach".
+        """
+        self.__seed = seed
+
+    def get_seed(self):
+        """
+        The function to get the current seed value.
+
+        Returns:
+            str: The current seed value.
+        """
+        return self.__seed
+
+    def set_seed(self, seed: str = "Peach"):
+        """
+        The function to set a new seed value.
+
+        Parameters:
+            seed (str): The new seed value. Default is "Peach".
+        """
+        self.__seed = seed
+
+    def __hashed(self, name: str = "default"):
+        """
+        The function to return a hashed value based on the seed and the name.
+        """
+
+        # generate md5 hash from the seed and the name
+        result = md5((self.__seed + name).encode()).hexdigest()
+        print("Seed: ", self.__seed, "Name: ", name, "Hash: ", result)
+        return result
+
+    def float(self, name: str = "default") -> float:
+        """
+        The function to return a 0-1 float value based on the seed and the name.
+        """
+
+        result = self.__hashed(name)
+
+        # convert the hash to a float value between 0 and 1
+        return int(result, 16) / 16**32
+
+
+    def bool(self, name: str = "default") -> bool:
+        """
+        The function to return a boolean value based on the seed and the name.
+        """
+
+        result = self.__hashed(name)
+        return int(result, 16) % 2 == 0
+
+    def int(self, name: str = "default", min: int = 0, max: int = 1_000_000) -> int:
+        """
+        The function to return an integer value based on the seed and the name.
+        The minimum value is inclusive and the maximum value is exclusive.
+        By default, the minimum value is 0 and the maximum value is 1,000,000.
+        """
+
+        result = self.__hashed(name)
+        return int(result, 16) % (max - min) + min
+
+
+if __name__ == "__main__":
+    # test the Seed class
+    seed = Seed("Peach")
+    for i in range(100):
+        print(seed.int("testing the seed trial #" + str(i)))
+        print(seed.bool("testing the seed trial #" + str(i)))
+        print(seed.float("testing the seed trial #" + str(i)))
+
