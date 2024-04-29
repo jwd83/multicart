8e5c0852534c98591a092b97d5383a15f196b098
Jared De Blander <jared@ayrscott.com>
2024-04-29 03:20:16 -0400

    cycling colors. generalized draw board

--- scenes/quadblox/quadblox.py
+++ scenes/quadblox/quadblox.py
@@ -20,21 +20,23 @@ class QuadBlox(Scene):
         if pygame.K_LEFT in self.game.just_pressed:
             for y, row in enumerate(self.player.grid):
                 for x, cell in enumerate(row):
-                    self.player.grid[y][x] -= 1
-    
+                    self.player.grid[y][x] = (self.player.grid[y][x] - 1) % len(colors)
+
         if pygame.K_RIGHT in self.game.just_pressed:
             for y, row in enumerate(self.player.grid):
                 for x, cell in enumerate(row):
-                    self.player.grid[y][x] += 1
-            
-            print(self.player.grid)
+                    self.player.grid[y][x] = (self.player.grid[y][x] + 1) % len(colors)
 
+        if pygame.K_UP in self.game.just_pressed:
+            print(self.player)
 
     def draw(self):
         self.screen.fill((0, 0, 0))
+        self.draw_board(self.player, (0, 0))
+
+    def draw_board(self, board, pos, block_size = 16):
 
         for y, row in enumerate(self.player.grid):
             for x, cell in enumerate(row):
                 if cell:
-                    pygame.draw.rect(self.screen, colors[cell], (x * 20, y * 20, 18, 18))
-
+                    pygame.draw.rect(self.screen, colors[cell], (x * block_size, y * block_size, block_size - 1, block_size - 1))
--- scenes/quadblox/scripts/qb.py
+++ scenes/quadblox/scripts/qb.py
@@ -1,8 +1,20 @@
+import random
+
 class Board:
     def __init__(self):
-        self.rows = 30
+        self.rows = 24
         self.cols = 10
+        self.grid = [[random.randint(1,len(colors) - 1) for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def clear(self):
         self.grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def reset(self):
+        self.clear()
+        self.grid = [[random.randint(1,len(colors) - 1) for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def __str__(self):
+        return "\n".join("".join(str(cell) for cell in row) for row in self.grid)
 
 colors = [
     (0, 0, 0),
