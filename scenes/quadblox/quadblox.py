9615369fafb74eb6a7f72032cefa551a68c0372e
Jared De Blander <jared0x90@gmail.com>
2024-05-01 16:42:53 -0400

    quadblox updates, mostly working single player

--- scenes/quadblox/quadblox.py
+++ scenes/quadblox/quadblox.py
@@ -1,82 +1,276 @@
-from numpy import block
 import pygame
 from scene import Scene
 from utils import *
-from .scripts.qb import Board, colors
+from .scripts.qb import Board, colors, Piece, Shapes
+import copy
 
 class QuadBlox(Scene):
     def __init__(self, game):
         super().__init__(game)
 
-        self.player = Board()
+
+        self.player_board = Board((100, 10))
+        self.player_board.clear()
 
         self.opponents = [Board() for _ in range(8)]
+        self.setup_opponents()
 
-        print(self.player.grid)
+        self.player_piece = Piece()
+        self.next_piece = Piece()
 
+        self.drop_at = 60 # frames per line fall
+        self.drop_count = 0
+
+        self.held_down_for = 0
+        self.held_toggles_at = 12
+        self.held_frame_interval = 4
+        self.held_left_for = 0
+        self.held_right_for = 0
+
+        self.died_at = 0
+
+        self.level = 0
+
+        self.standard_font_size = 20
+        self.standard_stroke = False
         
+
+
+    def setup_opponents(self):
+        for opponent in self.opponents:
+            # opponent.clear()
+
+            # setup to draw opponent boards
+            obs = 6
+            opponent.block_size = obs
+
+            x_step = obs * 11
+
+            y1 = 10
+            y2 = y1 + 28 * obs
+
+            x = 640 / 2
+            y = y1
+
+            for i, opponent in enumerate(self.opponents):
+
+                opponent.pos = (x, y)
+
+                if i % 2 == 0:
+                    y = y2
+                else:
+                    y = y1
+                    x += x_step
+
 
     def update(self):
         # if the user presses escape or F5 key, quit the event loop.
         if pygame.K_ESCAPE in self.game.just_pressed:
             self.game.scene_push = "Menu"
+            return
+        
+        
+        # if we are dead stop the update logic here
+        if self.died_at:
+            return 
+        
+        # check if we are already colliding. If we are, place the piece
+        if self.player_piece.collides(self.player_board):
+            self.place()
+            return
 
-        if pygame.K_LEFT in self.game.just_pressed:
-            for y, row in enumerate(self.player.grid):
-                for x, cell in enumerate(row):
-                    self.player.grid[y][x] = (self.player.grid[y][x] - 1) % len(colors)
 
-        if pygame.K_RIGHT in self.game.just_pressed:
-            for y, row in enumerate(self.player.grid):
-                for x, cell in enumerate(row):
-                    self.player.grid[y][x] = (self.player.grid[y][x] + 1) % len(colors)
+        # LEFT / RIGHT MOVEMENT AND ROTATION
+        left_held_tick = False
+        right_held_tick = False
+
+
+        # check if left has been held for a while 
+        if not self.game.pressed[pygame.K_LEFT]:
+            self.held_left_for = 0
+        else:
+            self.held_left_for += 1
+
+            if self.held_left_for >= self.held_toggles_at:
+                numerator = self.held_left_for - self.held_toggles_at
+                denominator = self.held_frame_interval
+
+                if numerator % denominator == 0:
+                    left_held_tick = True
+            
+        
+        if not self.game.pressed[pygame.K_RIGHT]:
+            self.held_right_for = 0
+        else:
+            self.held_right_for += 1
+
+            if self.held_right_for >= self.held_toggles_at:
+                numerator = self.held_right_for - self.held_toggles_at
+                denominator = self.held_frame_interval
+
+                if numerator % denominator == 0:
+                    right_held_tick = True
+            
+
+        sim_piece = copy.deepcopy(self.player_piece)
+        if pygame.K_LEFT in self.game.just_pressed or left_held_tick:
+            sim_piece.x -= 1
+
+        if pygame.K_RIGHT in self.game.just_pressed or right_held_tick:
+            sim_piece.x += 1
 
         if pygame.K_UP in self.game.just_pressed:
-            print(self.player)
+            sim_piece.rotate_and_size()
+
+
+
+
+        # if the sim piece does not collide update our piece to it
+        if not sim_piece.collides(self.player_board):
+            self.player_piece = sim_piece
+
+
+
+        # DOWN MOVEMENT / GRAVITY 
+        # should we fall this frame?
+        sim_drop = False
+    
+        # check if down has been held for a while
+        if self.game.pressed[pygame.K_DOWN]:
+            self.held_down_for += 1
+            if self.held_down_for >= self.held_toggles_at:
+                numerator = self.held_down_for - self.held_toggles_at
+                denominator = self.held_frame_interval
+
+                if numerator % denominator == 0:
+                    sim_drop = True
+        else:
+            self.held_down_for = 0
+
+        # check for key press down
+        if pygame.K_DOWN in self.game.just_pressed:
+            sim_drop = True
+
+        # check for gravity fall
+        self.drop_count += 1
+        if self.drop_count >= self.drop_at:
+            sim_drop = True
+    
+                
+        # increment drop count and see if we need to drop the piece
+        if sim_drop:
+            
+            # if we are dropping the piece, reset the drop count
+            self.drop_count = 0
+            drop_sim = copy.deepcopy(self.player_piece)
+            drop_sim.y += 1
+            if not drop_sim.collides(self.player_board):
+                self.player_piece = drop_sim
+            else:
+                self.place()
+
+        # check for death
+        for x in range(10):
+            for y in range(4):
+                if self.player_board.grid[y][x]:
+                    self.died_at = self.elapsed
+                    self.player_board.kill()
+                    return
+
+
+    def place(self):
+        # place the current piece and get a new piece
+        self.player_board.place(self.player_piece)
+        self.player_piece = self.next_piece
+        self.next_piece = Piece()
 
     def draw(self):
+
+        # draw the player board
         self.screen.fill((0, 0, 0))
-        self.draw_board(self.player, (100, 10))
+        self.draw_board(self.player_board)
 
-        opponent_block_size = 6
+        # draw the player stats
+        pos = self.player_board.pos
+        bs = self.player_board.block_size
 
-        x_step = opponent_block_size * 11
-
-        y1 = 10
-        y2 = y1 + 28 * opponent_block_size
-
-        x = 640 / 2
-        y = y1
-
-        for i, opponent in enumerate(self.opponents):
-  
-            self.draw_board(opponent, (x, y), opponent_block_size)
-
-            if i % 2 == 0:
-                y = y2
-            else:
-                y = y1
-                x += x_step          
-
-    def draw_board(self, board: Board, pos, block_size = 12):
-
-        # draw a red horizontal line after the first 4 rows        
-        pygame.draw.line(
-            self.screen, 
-            (255, 0, 0), 
-            (pos[0], pos[1] + 4 * block_size-1), 
-            (pos[0] + 10 * block_size -1, pos[1] + 4 * block_size -1)
+        self.screen.blit(
+            self.standard_text( str(self.player_board.points)),
+            (pos[0] + bs * 11, pos[1])
         )
 
+        self.screen.blit(
+            self.standard_text("CLEARS"),
+            (pos[0] + bs * 11, pos[1] + 20)
+        )
+
+        for y in range(4):
+            self.screen.blit(
+                self.standard_text(f"{y + 1}x " + str(self.player_board.clears[y])),
+                (pos[0] + bs * 11, pos[1] + 40 + y * 20)
+            )
+
+
+
+        # draw the opponents boards
+        for opponent in self.opponents:
+            self.draw_board(opponent)
+
+        # draw our piece
+        self.draw_piece()
+
+    def draw_piece(self):
+        for x in range(4):
+            for y in range(4):
+                if self.player_piece.grid[y][x]:
+                    pygame.draw.rect(
+                        self.screen,
+                        colors[self.player_piece.color],
+                        (
+                            (self.player_piece.x + x) * 12 + 100,
+                            (self.player_piece.y + y) * 12 + 10,
+                            12 - 1,
+                            12 - 1)
+                    )
+
+    def draw_board(self, board: Board):
+        pos = board.pos
+        bs = board.block_size
+
+        # draw a red horizontal line after the first 4 rows
+        pygame.draw.line(
+            self.screen,
+            (255, 0, 0),
+            (pos[0], pos[1] + 4 * bs-1),
+            (pos[0] + 10 * bs -1, pos[1] + 4 * bs -1)
+        )
+
+        # draw the board
         for y, row in enumerate(board.grid):
             for x, cell in enumerate(row):
                 if cell:
                     pygame.draw.rect(
-                        self.screen, 
-                        colors[cell], 
+                        self.screen,
+                        colors[cell],
                         (
-                            pos[0] + x * block_size, 
-                            pos[1] + y * block_size, 
-                            block_size - 1, 
-                            block_size - 1)
+                            pos[0] + x * bs,
+                            pos[1] + y * bs,
+                            bs - 1,
+                            bs - 1)
                     )
+
+        # draw a grey border around the board
+        border_width = 4
+        pygame.draw.rect(
+            surface=self.screen,
+            color=(128, 128, 128),
+            rect=(
+                pos[0] - border_width - 1,
+                pos[1] - border_width - 1,
+                10 * bs + 2 * border_width + 1,
+                24 * bs + 2 * border_width + 1
+            ),
+            width=border_width
+        )
+
+
--- scenes/quadblox/scripts/qb.py
+++ scenes/quadblox/scripts/qb.py
@@ -1,28 +1,211 @@
+from enum import Enum, auto
 import random
 
+# from https://colorkit.co/palette/ffadad-ffd6a5-fdffb6-caffbf-9bf6ff-a0c4ff-bdb2ff-ffc6ff/
+colors = [
+    (0, 0, 0),
+    '#ffadad',
+    '#ffd6a5', 
+    '#fdffb6', 
+    '#caffbf', 
+    '#9bf6ff', 
+    '#a0c4ff', 
+    '#bdb2ff', 
+    '#ffc6ff',
+    (255, 255, 255),
+ 
+]
+
+class Shapes(Enum):
+    I = auto()
+    J = auto()
+    L = auto()
+    O = auto()
+    S = auto()
+    T = auto()
+    Z = auto()
+
 class Board:
-    def __init__(self):
+    def __init__(self, pos=(0, 0), block_size=12):
         self.rows = 24
         self.cols = 10
+        self.pos = pos
         self.grid = [[random.randint(1,len(colors) - 1) for _ in range(self.cols)] for _ in range(self.rows)]
+        self.block_size = block_size
+
+        # scoring variables
+        self.points = 0
+        self.lines_cleared = 0
+        self.clears = [0, 0, 0, 0]
+        self.blocks_placed = 0
+
+    def place(self, piece):
+        self.blocks_placed += 1
+        for row in range(piece.height):
+            for col in range(piece.width):
+                if piece.grid[row][col]:
+                    self.grid[piece.y + row][piece.x + col] = piece.color
+        
+        self.score()
+
+    def score(self):
+        lines_cleared = 0
+        for row in range(self.rows):
+            if all(self.grid[row]):
+                lines_cleared += 1
+                self.grid.pop(row)
+                self.grid.insert(0, [0 for _ in range(self.cols)])
+
+        self.lines_cleared += lines_cleared
+    
+        if lines_cleared:    
+            self.clears[lines_cleared - 1] += 1
+
+        self.points = (
+            (   10 * self.clears[0]) + 
+            (  100 * self.clears[1]) + 
+            ( 1000 * self.clears[2]) + 
+            (10000 * self.clears[3]) + 
+            self.blocks_placed
+        )
+        
 
     def clear(self):
         self.grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
-    def reset(self):
-        self.clear()
+    def kill(self):
         self.grid = [[random.randint(1,len(colors) - 1) for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def reset(self):
+        self.score = 0
+        self.lines_cleared = 0
+        self.clears = [0, 0, 0, 0]
+        self.blocks_placed = 0
+        self.clear()
 
     def __str__(self):
         return "\n".join("".join(str(cell) for cell in row) for row in self.grid)
 
-colors = [
-    (0, 0, 0),
-    (255, 0, 0),
-    (0, 255, 0),
-    (0, 0, 255),
-    (255, 255, 0),
-    (255, 0, 255),
-    (0, 255, 255),
-    (255, 255, 255),
-]
+class Piece:
+
+    def __init__(self, shape: Shapes | None = None):
+
+        if shape is None:
+            shape = random.choice(list(Shapes))
+
+        # set our x and y start positions
+        self.x = 4
+        self.y = 0
+
+        self.height = 0
+        self.width = 0
+
+        # store which shape we are
+        self.shape = shape
+
+        # make our box 4x4
+        box_size = 4
+        self.grid = [[0 for _ in range(box_size)] for _ in range(box_size)]
+
+        # set our color
+        self.color = int(shape.value)
+        # set our shape
+        self.set_shape()
+
+        # rotate 1-4 times to normalize the shifting logic in rotate
+        # and set the height and width of the piece in the grid
+        rotations = random.randint(1, 4)
+        while rotations:
+            rotations -= 1
+            self.rotate_and_size()
+
+        print(f"New piece, Shape: {self.shape} Color: {self.color}, Height: {self.height}, Width: {self.width}")
+
+    def set_shape(self):
+        shape = self.shape
+        # populate our box with our shape
+        if shape == Shapes.I:
+            self.grid[0] = [1, 1, 1, 1]
+        elif shape == Shapes.J:
+            self.grid[0] = [1, 1, 1, 0]
+            self.grid[1] = [0, 0, 1, 0]
+        elif shape == Shapes.L:
+            self.grid[0] = [1, 1, 1, 0]
+            self.grid[1] = [1, 0, 0, 0]
+        elif shape == Shapes.O:
+            self.grid[1] = [0, 1, 1, 0]
+            self.grid[2] = [0, 1, 1, 0]
+        elif shape == Shapes.S:
+            self.grid[0] = [0, 1, 1, 0]
+            self.grid[1] = [1, 1, 0, 0]
+        elif shape == Shapes.T:
+            self.grid[0] = [1, 1, 1, 0]
+            self.grid[1] = [0, 1, 0, 0]
+        elif shape == Shapes.Z:
+            self.grid[0] = [1, 1, 0, 0]
+            self.grid[1] = [0, 1, 1, 0]
+
+    
+    def reverse_rotate_and_size(self):
+        self.rotate_and_size()
+        self.rotate_and_size()
+        self.rotate_and_size()
+
+
+    def rotate_and_size(self):
+        # make our box 4x4
+        box_size = 4
+        new_grid = [[0 for _ in range(box_size)] for _ in range(box_size)]
+
+
+        # rotate our shape in the new box
+        new_grid[0] = [self.grid[3][0], self.grid[2][0], self.grid[1][0], self.grid[0][0]]
+        new_grid[1] = [self.grid[3][1], self.grid[2][1], self.grid[1][1], self.grid[0][1]]
+        new_grid[2] = [self.grid[3][2], self.grid[2][2], self.grid[1][2], self.grid[0][2]]
+        new_grid[3] = [self.grid[3][3], self.grid[2][3], self.grid[1][3], self.grid[0][3]]
+
+
+        # shift the piece up so the top most row is not empty
+        while not any(new_grid[0]):
+            new_grid.pop(0)
+            new_grid.append([0, 0, 0, 0])
+
+        # shift the piece left so the left most column is not empty
+        while not any(row[0] for row in new_grid):
+            for row in new_grid:
+                row.pop(0)
+                row.append(0)
+
+        # calculate the block height and width
+        height = 0
+        width = 0
+
+        for row in range(4):
+            for col in range(4):
+                if new_grid[row][col]:
+                    height = max(height, row + 1)
+                    width = max(width, col + 1)
+        
+        print(f"Rotated piece, Shape: {self.shape} Color: {self.color}, Height: {height}, Width: {width}")
+        self.height = height
+        self.width = width
+ 
+        self.grid = new_grid
+
+    def collides(self, board):
+        # check that we are inside the bounds of the game world at all
+        if (
+            self.x < 0 or
+            self.x + self.width > board.cols or
+            self.y + self.height > board.rows
+        ):
+            return True
+
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.grid[row][col] and board.grid[self.y + row][self.x + col]:
+                    return True
+        return False
+
+    def __str__(self):
+        return "\n".join("".join(str(cell) for cell in row) for row in self.shape[self.rotation])
