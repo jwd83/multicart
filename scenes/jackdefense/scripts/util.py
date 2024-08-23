07606b648f3df543d636bae8fd65699b7cb7ea66
Kayla <kayla.marie.792@gmail.com>
2024-08-23 01:24:48 -0400

    formatting

--- scenes/jackdefense/scripts/util.py
+++ scenes/jackdefense/scripts/util.py
@@ -1,13 +1,17 @@
 import pygame
 import random
 
-class Deck():
+
+class Deck:
     def __init__(self):
         self.deck: list[str] = ["basic", "fire"]
 
-    def get_deck(self,):
+    def get_deck(
+        self,
+    ):
         return self.deck.copy()
 
-    def draw_from_deck(self,):
+    def draw_from_deck(
+        self,
+    ):
         return random.choices(self.deck)
-
