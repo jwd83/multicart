c32e7ab7310a15a53ad9eea2ee9d4b375ee6934f
Jared De Blander <jared0x90@gmail.com>
2025-01-10 18:35:38 -0500

    refactor game selection options to use a dictionary for improved readability and maintainability

--- scenes/gameselect.py
+++ scenes/gameselect.py
@@ -18,20 +18,27 @@ class GameSelect(Scene):
         self.text_multicart = self.standard_text("Jack Games Multicart")
         self.text_choose = self.standard_text("make a selection")
         self.standard_font_size = 15
-        self.options = [
-            self.standard_text("QuadBlox"),
-            self.standard_text("Jack Ninjas"),
-            self.standard_text("Jack Ninjas Editor"),
-            self.standard_text("Jack Wizards"),
-            self.standard_text("Julia"),
-            self.standard_text("Golden"),
-            self.standard_text("JackTD"),
-            self.standard_text("Blackjack"),
-            self.standard_text("Multi Test"),
-            self.standard_text("Warp"),
-            self.standard_text("Options"),
-            self.standard_text("Quit to Desktop"),
-        ]
+
+        self.opts = {
+            "QuadMenu": "QuadBlox",
+            "JackNinjasTitle": "Jack Ninjas",
+            "JackNinjasEditor": "Jack Ninjas Editor",
+            "JackWizards": "Jack Wizards",
+            "Julia": "Julia",
+            "Golden": "Golden",
+            "JackDefenseTitle": "JackTD",
+            "JackBlackJackTitle": "Blackjack",
+            "MultiTest": "Multi Test",
+            "Warp": "Warp",
+            "Menu": "Menu",
+            "Quit": "Quit to Desktop",
+        }
+
+        self.texts = {}
+
+        for opt in self.opts:
+            self.texts[opt] = self.standard_text(self.opts[opt])
+
         self.selected = 0
 
         # play the default music
@@ -42,58 +49,41 @@ class GameSelect(Scene):
             self.game.quit = True
 
         if pygame.K_DOWN in self.game.just_pressed:
-            self.selected = (self.selected + 1) % len(self.options)
+            self.selected = (self.selected + 1) % len(self.opts)
             self.play_sound("click")
 
         if pygame.K_UP in self.game.just_pressed:
-            self.selected = (self.selected - 1) % len(self.options)
+            self.selected = (self.selected - 1) % len(self.opts)
             self.play_sound("click")
 
         if pygame.K_RETURN in self.game.just_pressed:
-            if self.selected != len(self.options) - 1:
+            if self.selected != len(self.opts) - 1:
                 self.play_sound("jsxfr-select")
 
-            if self.selected == 0:
-                self.game.scene_replace = "QuadMenu"
-            elif self.selected == 1:
-                self.game.scene_replace = "JackNinjasTitle"
-            elif self.selected == 2:
-                self.game.scene_replace = "JackNinjasEditor"
-            elif self.selected == 3:
-                self.game.scene_replace = "JackWizards"
-            elif self.selected == 4:
-                self.game.scene_replace = "Julia"
-            elif self.selected == 5:
-                self.game.scene_replace = "Golden"
-            elif self.selected == 6:
-                self.game.scene_replace = "JackDefenseTitle"
-            elif self.selected == 7:
-                self.game.scene_replace = "JackBlackJackTitle"
-            elif self.selected == 8:
-                self.game.scene_replace = "MultiTest"
-            elif self.selected == 9:
-                self.game.scene_push = "Warp"
-            elif self.selected == 10:
-                self.game.scene_push = "Menu"
-            elif self.selected == 11 and not settings.WASM:
-                self.game.quit = True
+            selected_op = 0
+            for opt in self.opts:
+                if selected_op == self.selected:
+                    if opt == "Quit":
+                        self.game.quit = True
+                    elif opt == "Menu":
+                        self.game.scene_push = "Menu"
+                    else:
+                        self.game.scene_replace = opt
 
     def draw(self):
-        # choose a soothing sky blue
-        # self.screen.fill((18, 27, 180))
-        # self.screen.blit(self.background, (0, 0))
-
         y_spacing = self.standard_font_size + 5
 
         self.blit_centered(self.text_multicart, self.screen, (0.5, 0.1))
         self.blit_centered(self.text_choose, self.screen, (0.5, 0.2))
 
-        for i, option in enumerate(self.options):
+        i = 0
+        for txt in self.texts:
             if i == self.selected:
-                option.set_alpha(255)
+                self.texts[txt].set_alpha(255)
             else:
-                option.set_alpha(150)
-            self.screen.blit(option, (100, 100 + i * y_spacing))
+                self.texts[txt].set_alpha(150)
+            self.screen.blit(self.texts[txt], (100, 100 + i * y_spacing))
+            i += 1
 
         self.screen.blit(
             self.img_cursor,
