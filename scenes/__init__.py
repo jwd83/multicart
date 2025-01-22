5f8f67e0642bbe1e72a42f47fd979800005b73ee
Jared De Blander <jared0x90@gmail.com>
2025-01-22 17:08:34 -0500

    cleaned up init

--- scenes/__init__.py
+++ scenes/__init__.py
@@ -1,64 +1,38 @@
+from .backgrounds.fractal import Mandelbrot
+from .backgrounds.golden import Golden
+from .backgrounds.julia import Julia
+from .backgrounds.lavalamp import LavaLamp
+from .backgrounds.plasma import Plasma
+from .backgrounds.warp import Warp
+from .console import Console
+from .fonttest.fonttest import FontTest
+from .fourjacks.fourjacksgameboard import FourJacksGameBoard
+from .fourjacks.fourjacksgameover import FourJacksGameOver
+from .fourjacks.fourjackstitle import FourJacksTitle
+from .gameselect import GameSelect
+from .jackblackjack.jackblackjack import JackBlackJack
+from .jackblackjack.jackblackjacktitle import JackBlackJackTitle
+from .jackdefense.jackdefensegameboard import JackDefenseGameBoard
+from .jackdefense.jackdefensegameover import JackDefenseGameOver
+from .jackdefense.jackdefensetitle import JackDefenseTitle
+from .jackgames import JackGames
+from .jackninjas.jackninjas import JackNinjas
+from .jackninjas.jackninjaseditor import JackNinjasEditor
+from .jackninjas.jackninjasinventory import JackNinjasInventory
+from .jackninjas.jackninjastitle import JackNinjasTitle
+from .jackninjas.jackninjaswinner import JackNinjasWinner
+from .jackwizards.jackwizards import JackWizards
+from .jackwizards.jackwizardsmap import JackWizardsMap
+from .multitest.multitest import MultiTest
+from .quadblox.quadblox import QuadBlox
+from .quadblox.quadleadearboard import QuadLeaderboard
+from .quadblox.quadmenu import QuadMenu
+from .superball.superballfield import SuperBallField
+from .superball.superballtitle import SuperBallTitle
+from .tvstatic import TVStatic
+from .viagalactica.vialevel import ViaLevel
+from .viagalactica.viastarfield import ViaStarfield
+from .viagalactica.viatitle import ViaTitle
 from scenes.debug import Debug
 from scenes.logo import Logo
 from scenes.menu import Menu
-from .jackgames import JackGames
-from .gameselect import GameSelect
-from .tvstatic import TVStatic
-
-# backgrounds
-from .backgrounds.plasma import Plasma
-from .backgrounds.lavalamp import LavaLamp
-from .backgrounds.fractal import Mandelbrot
-from .backgrounds.julia import Julia
-from .backgrounds.golden import Golden
-from .backgrounds.warp import Warp
-
-# console
-from .console import Console
-
-# jack wizards
-from .jackwizards.jackwizards import JackWizards
-from .jackwizards.jackwizardsmap import JackWizardsMap
-
-# via galactica
-from .viagalactica.viatitle import ViaTitle
-from .viagalactica.viastarfield import ViaStarfield
-from .viagalactica.vialevel import ViaLevel
-
-# 4jacks
-from .fourjacks.fourjackstitle import FourJacksTitle
-from .fourjacks.fourjacksgameboard import FourJacksGameBoard
-from .fourjacks.fourjacksgameover import FourJacksGameOver
-
-# jackTD
-from .jackdefense.jackdefensetitle import JackDefenseTitle
-from .jackdefense.jackdefensegameboard import JackDefenseGameBoard
-from .jackdefense.jackdefensegameover import JackDefenseGameOver
-
-# font test
-from .fonttest.fonttest import FontTest
-
-# jackninjas
-
-from .jackninjas.jackninjas import JackNinjas
-from .jackninjas.jackninjastitle import JackNinjasTitle
-from .jackninjas.jackninjaseditor import JackNinjasEditor
-from .jackninjas.jackninjaswinner import JackNinjasWinner
-
-# superball
-from .superball.superballtitle import SuperBallTitle
-from .superball.superballfield import SuperBallField
-
-# quadblox
-from .quadblox.quadleadearboard import QuadLeaderboard
-from .quadblox.quadblox import QuadBlox
-from .quadblox.quadmenu import QuadMenu
-
-# multitest
-from .multitest.multitest import MultiTest
-
-# blackjack
-from .jackblackjack.jackblackjacktitle import JackBlackJackTitle
-from .jackblackjack.jackblackjack import JackBlackJack
-
-from .jackninjas.jackninjasinventory import JackNinjasInventory # auto-generated
